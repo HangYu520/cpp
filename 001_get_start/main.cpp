cbae33136590eeedc8cbb2d28841c670c92c9192
HangYu520 <HangYu520@users.noreply.github.com>
2025-09-25 03:42:00 +0800

    Create main.cpp

---
+++ 001_get_start/main.cpp
@@ -0,0 +1,19 @@
+/* 
+    The C++ coding environment: VS CODE + GCC + CMake
+
+    In this chapter, we must know:
+    
+    - how to complie and debug C++ code in the VS CODE ?
+
+    - how to write the CMAKELists file ?
+
+      see https://blog.csdn.net/iuu77/article/details/129229361 for more details
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
