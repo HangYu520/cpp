2abbbc596b4fc4810737f8c83e3a334c88bb60d2
HangYu520 <HangYu520@users.noreply.github.com>
2025-10-11 23:49:57 +0800

    021

---
+++ 021_pre_compiled_headers/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/Users/hangyu/Documents/GitHub/cpp/021_pre_compiled_headers/bin/main"
+  "/Users/hangyu/Documents/GitHub/cpp/021_pre_compiled_headers/bin/main.pdb"
+  "CMakeFiles/main.dir/cmake_pch.hxx.pch"
+  "CMakeFiles/main.dir/cmake_pch.hxx.pch.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 021_pre_compiled_headers/src/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <utility>
+
+// Data Structures
+#include <string>
+#include <format>
+#include <stack>
+#include <queue>
+#include <array>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
